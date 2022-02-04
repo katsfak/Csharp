07c75da347eeda72cff87668562b93e6a69c4acd
katsfak <57565765+katsfak@users.noreply.github.com>
2022-02-05 00:52:54 +0200

    Create name.cs
    show your name

---
+++ name.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Put your name: ");
+            name=Console.ReadLine();
+            
+            
+            Console.WriteLine("Hello {0}.",name);
+        }
+    }
+}
