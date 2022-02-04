361d868d0b4df66c136d8671b284b0440a3b6309
katsfak <57565765+katsfak@users.noreply.github.com>
2022-02-05 00:56:59 +0200

    show name and age

---
+++ age.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Name: "+ name);
+            Console.WriteLine("Age: "+ age);
+
+        }
+    }
+}
